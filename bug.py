8f7b70afac4363cbf7d2503a27ce22240d800905
Anirudh Ramchandran <aramchandran@tenstorrent.com>
2024-04-09 15:50:25 -0400

    now try without gpg key to ensure it works

--- bug.py
+++ bug.py
@@ -1,6 +1,7 @@
+
+from torch_df_from_str import torch_df_from_str
+from transformers import BertConfig, BertModel
 import os
 
 import pybuda
 import torch
-from torch_df_from_str import torch_df_from_str
-from transformers import BertConfig, BertModel
