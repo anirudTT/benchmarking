33980afc85f274a97b85071b1c50306ef175ee39
anirudTT <anirudTT@users.noreply.github.com>
2024-04-09 19:58:30 +0000

    *** AUTOMATED COMMIT | Applied Code Formatting and Cleanup ✨ 🍰 ✨***

--- bug.py
+++ bug.py
@@ -1,7 +1,6 @@
-
-from torch_df_from_str import torch_df_from_str
-from transformers import BertConfig, BertModel
 import os
 
 import pybuda
 import torch
+from torch_df_from_str import torch_df_from_str
+from transformers import BertConfig, BertModel
