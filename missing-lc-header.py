1c665d52b00ea8948171b2ede510e08fc9078f57
Anirudh Ramchandran <aramchandran@tenstorrent.com>
2024-04-12 09:15:10 -0400

    re do the workflow

--- missing-lc-header.py
+++ missing-lc-header.py
@@ -1,5 +1,5 @@
 # SPDX-FileCopyrightText: © 2024 Tenstorrent AI ULC
 # SPDX-License-Identifier: Apache-2.0
 
-print("have no licenses in this file")
-print("have no licenses in this file")
+print("correct lisnces in this file")
+
